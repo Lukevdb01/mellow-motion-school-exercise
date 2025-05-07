c6e1f07981239fe9095560a1247725ff897d0d8d
Luke <contact@lukevdbroek.nl>
2025-05-07 14:03:58 +0200

    Got animations working

--- src/scenes/game.scene.ts
+++ src/scenes/game.scene.ts
@@ -1,5 +1,6 @@
 import Scene from "@/core/Scene";
-
+import * as THREE from 'three';
+import sceneManager from "@/core/SceneManager";
 class Game extends Scene {
     /**
      *
@@ -8,9 +9,32 @@ class Game extends Scene {
         super("GameScene");
     }
 
-    override load(): void {
-        this.addMesh('models/Sponza/Sponza.gltf');
-        console.log("test");
-    }
+    environment() {
+        this.scene.background = new THREE.Color(0x87CEEB);
+        this.camera.position.set(0, 75, 75);
+        this.camera.lookAt(0, 0, 0);
+    
+        const geometry = new THREE.PlaneGeometry(100, 100);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+        const plane = new THREE.Mesh(geometry, material);
+    
+        plane.rotation.x = -Math.PI / 2;
+        this.scene.add(plane);
+    }    
+
+    override async load(): Promise<void> {
+        super.load();
+        this.environment();
+    
+        // Wait for the model to be loaded
+        const gltf = await this.addMesh('models/Crab.glb');
+
+        // Check if animations exist and create a mixer for them
+        if (this.mixer && gltf.animations.length > 0) {
+            gltf.animations.forEach((clip) => {
+                this.mixer.clipAction(clip).play();
+            });
+        }
+    }    
 }
 export default Game;
