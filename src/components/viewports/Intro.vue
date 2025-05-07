25cf0208df86c23fc51ca8937bd756b3bd5596aa
Abdul-Samed <68781362+SamedBozaslan@users.noreply.github.com>
2025-05-07 13:46:10 +0200

    .

--- src/components/viewports/Intro.vue
+++ src/components/viewports/Intro.vue
@@ -4,16 +4,23 @@ import {useRouter} from "vue-router";
 
 const showRedBlock = ref(true);
 const router = useRouter();
+const counter = ref(10); // Initialize counter to 3 seconds
 
 onMounted(() => {
-  setTimeout(() => {
-    router.push('/game-container');
-  }, 5000); // 10 seconds
+  const interval = setInterval(() => {
+    counter.value -= 1;
+    if (counter.value <= 0) {
+      clearInterval(interval);
+      router.push('/game-container');
+    }
+  }, 1000);
 });
 </script>
 
 <template>
   <section v-if="showRedBlock" class="h-screen w-screen overflow-hidden z-10">
-    <div class="w-full h-full bg-red-500"></div>
+    <div class="w-full h-full bg-red-500 flex items-center justify-center text-white text-6xl">
+      {{ counter }}
+    </div>
   </section>
 </template>
