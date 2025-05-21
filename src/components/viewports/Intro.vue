a9ff67d496dc4568188cc98dc1d322418259757c
Abdul-Samed <68781362+SamedBozaslan@users.noreply.github.com>
2025-05-21 15:46:17 +0200

    Added svgs

--- src/components/viewports/Intro.vue
+++ src/components/viewports/Intro.vue
@@ -24,19 +24,14 @@
 
   <template>
     <section class="h-screen w-screen flex justify-center items-center bg-amber-100 text-white text-6xl">
-      <div v-if="!hideContent" :class="['flex w-full h-full justify-between p-16 transition-opacity duration-500', fadeOut ? 'opacity-0' : 'opacity-100']">
+      <div v-if="!hideContent" :class="['flex w-full h-full justify-between px-8 py-12 transition-opacity duration-500', fadeOut ? 'opacity-0' : 'opacity-100']">
         <div class="flex flex-col justify-between h-full">
-          <img src="/image/memo%20logo%20zwart.png" alt="Logo" class="w-40">
-          <div class="flex items-end gap-12">
-            <button class="font-bold px-12 py-10 bg-blue-600 text-white rounded hover:bg-blue-700 text-5xl cursor-pointer">
-              CREDITS
-            </button>
-            <button
-                @click="start"
-                class="font-bold px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 text-5xl cursor-pointer"
-            >
-              START
-            </button>
+          <img src="/image/Titel.svg" alt="Logo" class="w-72 rotate-350">
+          <div class="flex items-end gap-6">
+            <p class="font-bold text-lg w-96 text-black">
+              Mellow Motion wilt in dit project aandacht richten op hoe schoonheidisdealen invloed hebben op de mentale gezondheid van jongeren. Wij nemen jullie mee in het leven van een leeftijdsgenoot die in contact komt met de digitale wereld. Jouw keuzes beinvloeden het verloop van dit verhaal en de mentale staat van ons hoofdpersonage. Klik om door het verhaal heen te gaan!
+            </p>
+              <img @click="start" src="/image/Start%20button%202.svg" alt="" class="cursor-pointer w-96">
           </div>
         </div>
         <div>
