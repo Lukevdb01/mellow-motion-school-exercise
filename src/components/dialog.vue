4ee360078a40cbb72b0925bdbc915024de0ca3f3
Luke <contact@lukevdbroek.nl>
2025-05-28 12:24:12 +0200

    added dialog animation

--- src/components/dialog.vue
+++ src/components/dialog.vue
@@ -5,17 +5,29 @@ const toggleDialog = ref(false);
 </script>
 
 <template>
-
   <div>
-    <div v-if="toggleDialog" class="bg-white max-w-[160px] p-4 rounded-[2vw] shadow-lg max-w-md mx-auto text-center">
-      <div class="text-lg">
-        <slot></slot>
+    <Transition name="fade">
+      <div v-if="toggleDialog" class="bg-white max-w-[160px] p-4 rounded-[2vw] shadow-lg max-w-md mx-auto text-center">
+        <div class="text-lg">
+          <slot></slot>
+        </div>
       </div>
-    </div>
+    </Transition>
     <div @click="toggleDialog = !toggleDialog" class="rounded-full bg-white my-4 h-[44px] w-[44px] flex items-center justify-center">
       <img src="/images/question.svg" alt="Question Icon" class="w-8 h-8 mx-auto" />
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Define the fade transition styles */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
